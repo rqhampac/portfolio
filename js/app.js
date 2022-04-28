c5177175c33f800990d9fd2bb044036adac4dd14
rqhampac <rqhampac@gmail.com>
2022-04-28 10:36:35 +0800

    added button rippling effect

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ const navBtn = document.querySelector("#nav-btn");
 const closeBtn = document.querySelector("#close-btn");
 const sidebar = document.querySelector("#sidebar");
 const date = document.querySelector("#date");
+const btnEl = document.querySelectorAll(".btn");
+
 // add fixed class to navbar
 window.addEventListener("scroll", function () {
   if (window.pageYOffset > 80) {
@@ -20,3 +22,14 @@ closeBtn.addEventListener("click", function () {
 });
 // set year
 date.innerHTML = new Date().getFullYear();
+
+//button ripple effect
+for(let i = 0, len = btnEl.length; i < len; i++){
+  btnEl[i].addEventListener("mouseover",(event)=>{
+    const x = (event.pageX - btnEl[i].offsetLeft);
+    const y = (event.pageY - btnEl[i].offsetTop);
+    btnEl[i].style.setProperty("--xPos",x + "px");
+    btnEl[i].style.setProperty("--yPos",y + "px");
+  })
+}
+

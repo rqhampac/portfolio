e0b6ceeb906fad472a0fa8016d5962145ecace24
rqhampac <rqhampac@gmail.com>
2022-04-29 15:58:29 +0800

    update projects page

--- js/app.js
+++ js/app.js
@@ -28,6 +28,7 @@ btnEls.forEach(btnEl=>{
   btnEl.addEventListener("mouseover",(event)=>{
     const x = (event.pageX - btnEl.offsetLeft);
     const y = (event.pageY - btnEl.offsetTop);
+    console.log(x,y);
     btnEl.style.setProperty("--xPos",x + "px");
     btnEl.style.setProperty("--yPos",y + "px");
   })
